848bc3e6741edc583c121b5242c581a5136b8309
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-05-01 21:23:27 -0300

    feat: Adiciona CORS e comandos de app do Discord

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using NetCord;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Hosting.Services.Commands;
 using Serilog;
 using Serilog.Events;
 
@@ -8,7 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
+#if !DEBUG
     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+#endif
     .Enrich.FromLogContext()
     .WriteTo.Console()
     .CreateLogger();
@@ -17,26 +21,44 @@ builder.Host.UseSerilog();
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
 builder.Services.AddDiscordGateway(options =>
 {
-   options.PublicKey = builder.Configuration["Discord:PublicKey"];
-   options.Token = builder.Configuration["Discord:Token"];
-   options.AutoStartStop = true;
-   options.Intents = GatewayIntents.Guilds | 
-   GatewayIntents.GuildMessages |
-   GatewayIntents.DirectMessages | 
-   GatewayIntents.MessageContent | 
-   GatewayIntents.DirectMessageReactions | 
-   GatewayIntents.GuildMessageReactions;
-   options.Presence = new PresenceProperties(UserStatusType.Online).WithActivities([
-       new UserActivityProperties("Coding with NetCord", UserActivityType.Competing)
-   ]);
+    options.PublicKey = builder.Configuration["Discord:PublicKey"];
+    options.Token = builder.Configuration["Discord:Token"];
+    options.AutoStartStop = true;
+    options.Intents = GatewayIntents.Guilds |
+    GatewayIntents.GuildMessages |
+    GatewayIntents.DirectMessages |
+    GatewayIntents.MessageContent |
+    GatewayIntents.DirectMessageReactions |
+    GatewayIntents.GuildMessageReactions;
+
+    options.Presence = new PresenceProperties(UserStatusType.Online).WithActivities([
+        new UserActivityProperties("Coding with NetCord", UserActivityType.Competing)
+    ]);
 });
+
+builder.Services.AddApplicationCommands(options =>
+{
+    options.AutoRegisterCommands = true;
+});
+
 builder.Services.AddGatewayHandlers(typeof(Program).Assembly);
 
 var app = builder.Build();
 
 app.UseSerilogRequestLogging();
+app.UseCors();
+app.MapControllers();
 
 if (app.Environment.IsDevelopment())
 {
